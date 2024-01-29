124a8d20637ce985126d426be8bfdb3fce267445
marcelo-conti02 <marceloconti9017@gmail.com>
2024-01-29 11:02:22 -0300

    Integra a chamada à API

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+internal class Program
+{
+    static void Main()
+    {
+        APIAlphaVantage alphaVantage = new APIAlphaVantage();
+
+        alphaVantage.APIcall();
+    }
+}
+
+abstract class APISystem
+{
+    public abstract void APIcall();
+}
+
+class APIAlphaVantage : APISystem
+{
+    public override void APIcall()
+    {
+        // Aguarda a conclusão da chamada à API
+        Call().Wait();
+    }
+
+    private static async Task Call()
+    {
+        // Chave de acesso da API e ativo da B3 para ser monitorado
+        string apiKey = "";
+        string asset = "AAPL";
+
+        string apiUrl = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={asset}&interval=1min&apikey={apiKey}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            // Envia uma requisição para a API
+            HttpResponseMessage requisition = await client.GetAsync(apiUrl);
+
+            if (requisition.IsSuccessStatusCode)
+            {
+                string reqResponse = await requisition.Content.ReadAsStringAsync();
+                Console.WriteLine(reqResponse);
+            }
+            else
+            {
+                Console.WriteLine($"Erro: {requisition.StatusCode}");
+            }
+        }
+    }
+}
