0569dcacc00ffd3ccfbb4a1d076eaa97d347ee72
marcelo-conti02 <marceloconti9017@gmail.com>
2024-01-31 16:28:17 -0300

    Implementa o retorno assíncrono da cotação do ativo

--- Program.cs
+++ Program.cs
@@ -5,33 +5,35 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        // Verifica se o número de parâmetros está correto
+        // Usuário precisa fornecer 3 parâmetros: ativo, preço referência de compra e preço referência de venda 
         if (args.Length != 3)
         {
             Console.WriteLine("Número incorreto de parametros.");
             return;
         }
-
+   
         APIAlphaVantage alphaVantage = new APIAlphaVantage();
         EmailNotification emailNotification = new EmailNotification();
         StockQuoteAlert stockQuoteAlert = new StockQuoteAlert(alphaVantage, emailNotification, args[0], double.Parse(args[1], CultureInfo.InvariantCulture), double.Parse(args[2], CultureInfo.InvariantCulture));
-        stockQuoteAlert.Alert();
+        stockQuoteAlert.Alert().Wait();
     }
 }
 
 class StockQuoteAlert(APISystem api, NotificationSystem notification, string asset, double minPrice, double maxPrice)
 {
-    public void Alert()
+    double price;
+    public async Task Alert()
     {
-        // Loop para verificar o ativo e tomar uma decisão
-        while(true)
-        {
-            // price = 
-            api.CheckPrice(asset);
+        //while(true)
+        //{
+            price = await api.CheckPrice(asset);
+
+            Console.WriteLine(price);
 
             if (price > maxPrice)
             {
@@ -41,62 +43,49 @@ class StockQuoteAlert(APISystem api, NotificationSystem notification, string ass
             {
                 notification.Notification("buy");
             }
-        }
+        //}
     }
 }
 
 abstract class APISystem
 {
-    public abstract void CheckPrice(string asset);
+    public abstract Task<double> CheckPrice(string asset);
 }
 
 class APIAlphaVantage : APISystem
 {
-    public override void CheckPrice(string asset)
+    public override async Task<double> CheckPrice(string asset)
     {
-        // Aguarda a conclusão da chamada à API
-        CallAPI(asset).Wait();
-
-        // Retorna o valor da cotação 
-        //return price;
+        double price = await CallAPI(asset);
+        return price;
     }
 
-    private static async Task CallAPI(string asset)
+    private static async Task<double> CallAPI(string asset)
     {
-        // Chave de acesso da API
         string apiKey = "";
         string apiUrl = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={asset}&interval=1min&apikey={apiKey}";
 
         using (HttpClient client = new HttpClient())
         {
-            // Envia uma requisição para a API
             HttpResponseMessage requisition = await client.GetAsync(apiUrl);
-
+          
             if (requisition.IsSuccessStatusCode)
             {
-                // Lê a resposta 
                 string apiResponse = await requisition.Content.ReadAsStringAsync();
-
-                // JSON para objeto
                 var data = JsonConvert.DeserializeObject<JObject>(apiResponse);
 
-                // Separa os dados da chave Time Series
+                // Separa os dados que são atualizados a cada 1min
                 var timeSeries = data["Time Series (1min)"] as JObject;
-
-                // Dados mais recentes do Time Series
                 var recentData = timeSeries.First.First;
 
-                // Armazena a cotação do ativo
+                // Separa o preço de fechamento do ativo para ser monitorado
                 var price = recentData["4. close"].ToString();
-                double.Parse(price);
-                
-                //Retorna a cotação
-                // return price
-
+                return double.Parse(price, CultureInfo.InvariantCulture);
             }
             else
-            {
+            { 
                 Console.WriteLine($"Erro: {requisition.StatusCode}");
+                return -1;
             }
         }
     }
@@ -114,9 +103,9 @@ class EmailNotification : NotificationSystem
         string sender = "";
         string recipient = "";
         string password = "";
+        string stmpClient = "smtp-mail.outlook.com";
         MailMessage message;
 
-        // Mensagem do e-mail
         if(warning == "sell")
         {
             message = new MailMessage(sender, recipient, "Cotação do ativo da B3", "Prezados, a cotação do ativo está acima do nível de referência para venda. Recomendo a venda.");
@@ -125,14 +114,12 @@ class EmailNotification : NotificationSystem
         {
             message = new MailMessage(sender, recipient, "Cotação do ativo da B3", "Prezados, a cotação do ativo está abaixo do nível de referência para compra. Recomendo a compra.");
         }
-   
-        // Configurações do cliente SMTP
-        SmtpClient client = new SmtpClient("smtp-mail.outlook.com");
+
+        SmtpClient client = new SmtpClient(stmpClient);
         client.Port = 587;
         client.Credentials = new NetworkCredential(sender, password);
         client.EnableSsl = true;
 
-        // Envia o e-mail
         client.Send(message);
         Console.WriteLine("E-mail enviado.");
     }
